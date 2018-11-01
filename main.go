cc633da0791c3da1724b429a6f398129dc41ba14
Alexander Grimm <alex-grimm@web.de>
2018-11-01 19:39:21 +0100

    Add small webservice whith minimal functionality to main.go. Returns jsons or Error codes if 'id' not in range. This is mainly for trying Go and the Mux router. Actual routes will change, espescially when integrating RabbitMQ. This implies starting work on #8

--- main.go
+++ main.go
@@ -1,22 +1,59 @@
 package main
 
 import (
-	"net/http"
-	"log"
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strconv"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
+type Content struct {
+	Question string
+	Answer   string
+}
 
+func dummyContents() map[uint]Content {
+	m := make(map[uint]Content)
+	m[1] = Content{"How is life these days?", "So good"}
+	m[2] = Content{"Are 2 questions sufficient?", "I do not think so!"}
+	m[3] = Content{"Are 3 questions sufficient?", "I think so!"}
+	return m
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Gorilla!\n"))
+	w.Write([]byte("Hello There!\n"))
+}
 
+func GetAllHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	j, _ := json.Marshal(dummyContents())
+	w.Write(j)
+}
+
+func GetByIdHandler(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	content, ok := dummyContents()[uint(id)]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "There is no id #%d", id)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		j, _ := json.Marshal(content)
+		w.Write(j)
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/all", GetAllHandler).Methods("GET")
+	r.HandleFunc("/get/{id:[0-9]+}", GetByIdHandler).Methods("GET")
+
+	r.HandleFunc("/", RootHandler)
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
