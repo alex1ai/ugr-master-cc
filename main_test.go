c1eb79d99f908889113b503872238f297263f9bc
Alexander Grimm <alex-grimm@web.de>
2018-11-05 23:03:39 +0100

    Add basic tests for all api methods, closes #10

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -12,7 +13,7 @@ import (
 func TestRootHandler(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
 	// pass 'nil' as the third parameter.
-	req, err := http.NewRequest("GET", "/health", nil)
+	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -32,7 +33,7 @@ func TestRootHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `Hello There!`
+	expected := `Welcome to the infogration REST-API!`
 	if strings.Trim(rr.Body.String(), "\n") != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
@@ -44,12 +45,9 @@ func TestGetByIdHandler(t *testing.T) {
 		variable   string
 		shouldPass bool
 	}{
-		{"1", true},
-		{"2", true},
-		{"3", true},
-		{"4", false},
-		{"asdf", false},
-
+		{"de", false},
+		{"en", true},
+		{"es", true},
 	}
 
 	for _, tc := range tt {
@@ -63,7 +61,7 @@ func TestGetByIdHandler(t *testing.T) {
 
 		// Need to create a router that we can pass the request through so that the vars will be added to the context
 		router := mux.NewRouter()
-		router.HandleFunc("/get/{id}", GetByIdHandler)
+		router.HandleFunc("/get/{code}", GetByLangHandler)
 		router.ServeHTTP(rr, req)
 
 		// In this case, our MetricsHandler returns a non-200 response
@@ -73,5 +71,38 @@ func TestGetByIdHandler(t *testing.T) {
 				tc.variable, rr.Code, http.StatusOK)
 		}
 
+	}
+}
+
+func TestGetAllHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/all", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetAllHandler)
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+
+	var responseObject JSONResponse
+	err = json.Unmarshal(rr.Body.Bytes(), &responseObject)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Check the response body is what we expect.
+	if len(responseObject.Data) != 4 {
+		t.Errorf("handler returned unexpected length: got %d want %d",
+			rr.Body, 4)
 	}
 }
